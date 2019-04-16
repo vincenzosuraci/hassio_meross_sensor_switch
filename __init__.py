ac6583ab96ea4c73b3564266c896e6cd6a54efe0
Vincenzo Suraci <suraci@diag.uniroma1.it>
2019-04-16 23:53:41 +0200

    Added support to switch and sensor removal.

--- __init__.py
+++ __init__.py
@@ -230,18 +230,17 @@ async def async_setup(hass, config):
 
         """ Delete no more existing Meross devices and related entities """
         """ Removal of entities seems to not work... """
-        #meross_device_ids_to_be_removed = []
-        #for meross_device_id in hass.data[DOMAIN][MEROSS_DEVICES_BY_ID]:
-        #    if meross_device_id not in hass.data[DOMAIN][MEROSS_LAST_DISCOVERED_DEVICE_IDS]:
-        #        meross_device_ids_to_be_removed.append(meross_device_id)
-        #        meross_device = hass.data[DOMAIN][MEROSS_DEVICES_BY_ID][meross_device_id][MEROSS_DEVICE]
-        #        meross_device_name = str(meross_device)
-        #        l.debug('Meross device '+meross_device_name+' is no more online and will be deleted')
-        #        for entity_id in hass.data[DOMAIN][MEROSS_DEVICES_BY_ID][meross_device_id][HA_ENTITY_IDS]:
-        #            dispatcher_send(hass, SIGNAL_DELETE_ENTITY, entity_id)
-
-        #for meross_device_id in meross_device_ids_to_be_removed:
-        #    hass.data[DOMAIN][MEROSS_DEVICES_BY_ID].pop(meross_device_id)
+        meross_device_ids_to_be_removed = []
+        for meross_device_id in hass.data[DOMAIN][MEROSS_DEVICES_BY_ID]:
+            if meross_device_id not in hass.data[DOMAIN][MEROSS_LAST_DISCOVERED_DEVICE_IDS]:
+                meross_device_ids_to_be_removed.append(meross_device_id)
+                meross_device = hass.data[DOMAIN][MEROSS_DEVICES_BY_ID][meross_device_id][MEROSS_DEVICE]
+                meross_device_name = str(meross_device)
+                l.debug('Meross device '+meross_device_name+' is no more online and will be deleted')
+                for entity_id in hass.data[DOMAIN][MEROSS_DEVICES_BY_ID][meross_device_id][HA_ENTITY_IDS]:
+                    dispatcher_send(hass, SIGNAL_DELETE_ENTITY, entity_id)
+        for meross_device_id in meross_device_ids_to_be_removed:
+            hass.data[DOMAIN][MEROSS_DEVICES_BY_ID].pop(meross_device_id)
 
     """ This is used to update the Meross Device list periodically """
     l.debug('registering async_track_time_interval(hass, async_poll_devices_update, meross_devices_scan_interval)')
@@ -309,10 +308,10 @@ class MerossDevice(Entity):
         return self.hass.data[DOMAIN][MEROSS_DEVICES_BY_ID][self.meross_device_id][MEROSS_DEVICE]
 
     @callback
-    def _delete_callback(self, meross_device_id):
+    def _delete_callback(self, entity_id):
         """Remove this entity."""
-        l.debug('Entity '+self.entity_id+' >>> _delete_callback()')
-        if meross_device_id == self.meross_device_id:
+        if entity_id == self.entity_id:
+            l.debug('Entity ' + self.entity_id + ' >>> _delete_callback()')
             self.hass.async_create_task(self.async_remove())
 
     @callback
